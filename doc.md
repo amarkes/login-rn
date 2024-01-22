9841547842fcc8e83a0629267b8948c53f15973c
Antonio Marques <tmowna@gmail.com>
2024-01-22 11:30:16 -0300

    init doc and install mmkv erros

---
+++ doc.md
@@ -0,0 +1,6 @@
+# Engine
+- RN 0.73.2
+- node 19.9.0
+
+# dependences
+- MMKV [https://github.com/mrousavy/react-native-mmkv]
---
+++ src/pages/home/styles.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default styles;
